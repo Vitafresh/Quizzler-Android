012215e9c2a2c972e44c681b9e6441078fbeb557
Vitafresh <vvitafresh@gmail.com>
2019-02-01 21:31:18 +0200

    Lesson 81 (Retrieve Question from an Array)

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Debug;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.Console;
 
@@ -17,23 +20,27 @@ public class MainActivity extends Activity {
         Button mTrueButton;
         Button mFalseButton;
 
+        TextView mQuestionTextView;
+        int mIndexQuestion = 0;
+        int mQuestionID;
+
 
     // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +50,35 @@ public class MainActivity extends Activity {
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
 
-        double x = 5.99;
-        int y = (int)x;
-        Log.d("DebInfo","y=" + y);
+        mQuestionTextView=(TextView)findViewById(R.id.question_text_view);
+
+
+//        TrueFalse firstQuestion = mQuestionBank[mIndexQuestion];
+//        int questionID = firstQuestion.getmQuestionID();
+//        mQuestionTextView.setText(questionID);
+        mQuestionTextView.setText(mQuestionBank[mIndexQuestion].getmQuestionID());
+
+
+        View.OnClickListener myOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("DebugInfo","Listener: True button pressed");
+
+                Toast myToast = Toast.makeText(getApplicationContext(),"True button pressed",Toast.LENGTH_LONG);
+                myToast.show();
+            }
+        };
+
+        //Set callback to buttons
+        mTrueButton.setOnClickListener(myOnClickListener);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("DebugInfo","Listener: False button pressed");
+                Toast.makeText(getApplicationContext(),"False button pressed",Toast.LENGTH_LONG).show();
+            }
+        });
+
 
 
     }
---
+++ app/src/main/java/com/londonappbrewery/quizzler/TrueFalse.java
@@ -0,0 +1,29 @@
+package com.londonappbrewery.quizzler;
+
+public class TrueFalse {
+    private int mQuestionID;
+    private boolean mAnswer;
+
+    public TrueFalse(int questionResID, boolean trueOrFalse){
+        mQuestionID=questionResID;
+        mAnswer=trueOrFalse;
+
+    }
+    
+
+    public int getmQuestionID() {
+        return mQuestionID;
+    }
+
+    public void setmQuestionID(int mQuestionID) {
+        this.mQuestionID = mQuestionID;
+    }
+
+    public boolean ismAnswer() {
+        return mAnswer;
+    }
+
+    public void setmAnswer(boolean mAnswer) {
+        this.mAnswer = mAnswer;
+    }
+}
